c230481814227f50052a5d103920759a1b614c92
Anacarolina1002 <carolquaglioto@outlook.com>
2020-11-03 15:42:45 -0200

    :star: fix: atividade parte.1

--- atividade1.py
+++ atividade1.py
@@ -1,17 +1,17 @@
-listaNumeros = [1,2,3,4]
+listaNumeros = [1,2,3,4]#declaração das coisinhas do programa
 numero=0
-def rotacaoLista(lista, numero):
+def rotacaoLista(lista, numero):#definição de uma def com dois argumentos
     numero=int(input("Insira um número positivo ou negativo: "))
     if numero >= 0:
         for i in range(numero):
-            numeroFim = lista.pop(-1)
-            lista.insert(0, numeroFim)
+            numeroFim = lista.pop(-1)#nessa parte, se um número é positivo, ele decremente o número final e adiciona ele no começo
+            lista.insert(0, numeroFim)#no começo, na posição 0, no caso.
     else:
         for i in range(abs(numero)):
-            numeroInicio = lista.pop(0)
+            numeroInicio = lista.pop(0)#aqui ele decrementa do início e adiciona no final.
             lista.append(numeroInicio)
             return
-rotacaoLista(listaNumeros, numero)
+rotacaoLista(listaNumeros, numero)#Inicia o programa
 print (listaNumeros) #[1, 2, 3, 4, 5, 6]
     
 
